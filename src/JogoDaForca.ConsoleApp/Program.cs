9de7c3967fe5fb29ed82a6e603d86bf4a76f6f47
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-04-03 17:05:34 -0300

    impput Usuario e imprimir a palavra

--- src/JogoDaForca.ConsoleApp/Program.cs
+++ src/JogoDaForca.ConsoleApp/Program.cs
@@ -4,7 +4,90 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string aleatorio = "ABACATE";
+            char[] palavraSecreta = new char[aleatorio.Length];
+            char [] palavra = aleatorio.ToCharArray();
+            int cont = 5;
+            /*
+            Console.Write(" ----------");
+            Console.Write(" \n |/ \n | \n | \n | \n | \n | \n |");
+            Console.Write("\n_|____");
+
+            Console.WriteLine("\n");
+           */
+
+            
+            for (int i = 0; i < aleatorio.Length; i++)
+            {
+                
+                Console.Write("_");
+               
+            }
+            Console.WriteLine();
+            
+            while (cont!=0)
+            {
+                Console.WriteLine();
+                char chute = ObterChute();
+                bool erro= AvaliarChute(aleatorio, palavraSecreta, palavra, ref cont, chute);
+               
+               if (erro == false) {
+
+                    if (cont == 4)
+                        Console.WriteLine(" O");
+                    else if (cont == 3)
+                        Console.WriteLine("(x)");
+                    else if (cont == 2)
+                        Console.WriteLine(" X");
+                    else if (cont == 1)
+                        Console.Write("/");
+                    else if (cont == 0)
+                        Console.WriteLine("| ");
+                }
+
+                for (int i = 0; i < aleatorio.Length; i++)
+                {
+                    Console.Write($"{palavraSecreta[i]}");
+                    if (palavraSecreta[i] == 0)
+                        Console.Write($"_");
+
+                }
+
+                Console.WriteLine();
+
+                
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static bool AvaliarChute(string aleatorio, char[] palavraSecreta, char[] palavra, ref int cont, char chute)
+        {
+            for (int i = 0; i < aleatorio.Length; i++)
+            {
+
+                if (chute == palavra[i])
+                {
+                    palavraSecreta[i] = chute;
+                    return true;
+                }
+
+
+
+            }
+            cont--;
+            return false;
+        }
+
+
+        private static char ObterChute()
+        {
+            Console.Write("Qual é seu chute? ");
+            char chute = Convert.ToChar(Console.ReadLine());
+            return chute;
         }
     }
 }
